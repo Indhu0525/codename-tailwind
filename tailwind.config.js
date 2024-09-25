fe64aa398bb7351b710ca2a7dae14690b6f0b689
Indhu0525 <indu01401@hmail.com>
2024-09-25 18:53:54 +0530

    content

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+       sidebarbg:'rgb(241 229 229)'
+      }
+    },
+  },
+  plugins: [],
+}
+
